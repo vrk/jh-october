f7d8fe7a566f7e8925c54c7c0ca115144c92beea
vrk <victoriakirst@gmail.com>
2024-10-12 12:19:27 -0400

    add top and bottom nav bars

--- src/components/JournalPageNav/JournalPageNav.tsx
+++ src/components/JournalPageNav/JournalPageNav.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import style from './JournalPageNav.module.css';
 
 function JournalPageNav() {
-  return <div className={style.container}></div>;
+  return <div className={style.container}>hi</div>;
 }
 
 export default JournalPageNav;
