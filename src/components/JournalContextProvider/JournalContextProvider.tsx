071f7dc9e464a415c85f543314fdc57b8580e83c
vrk <victoriakirst@gmail.com>
2024-10-23 17:16:15 -0400

    move journal info to context

--- src/components/JournalContextProvider/JournalContextProvider.tsx
+++ src/components/JournalContextProvider/JournalContextProvider.tsx
@@ -6,12 +6,19 @@ import {
   SpreadItem,
 } from "@/helpers/indexdb";
 import React from "react";
+import {
+  createImageResourceForJournal,
+  getImagesForJournalWithUsageInformation,
+  JournalImage,
+} from "@/helpers/indexdb";
 
 type JournalContextType = {
   journalId: string | null;
   currentSpreadId: string | null;
   allSpreads: Array<Spread>;
   currentSpreadItems: Array<SpreadItem>;
+  loadedImages: Array<JournalImage>;
+  setLoadedImages: (loadedImages: Array<JournalImage>) => void;
   setCurrentSpreadId: (currentSpreadId: string) => void;
   setAllSpreads: (allSpreads: Array<Spread>) => void;
   setCurrentSpreadItems: (currentSpreadItemIds: Array<SpreadItem>) => void;
@@ -26,6 +33,8 @@ export const JournalContext = React.createContext<JournalContextType>({
   currentSpreadId: null,
   allSpreads: [],
   currentSpreadItems: [],
+  loadedImages: [],
+  setLoadedImages: () => {},
   setCurrentSpreadId: () => {},
   setAllSpreads: () => {},
   setCurrentSpreadItems: () => {},
@@ -41,6 +50,10 @@ const JournalContextProvider = ({
   const [currentSpreadItems, setCurrentSpreadItems] = React.useState<
     Array<SpreadItem>
   >([]);
+  // TODO: Check perf of this
+  const [loadedImages, setLoadedImages] = React.useState<Array<JournalImage>>(
+    []
+  );
 
   React.useEffect(() => {
     getAllSpreadsForJournal(journalId).then((spreads) => {
@@ -60,6 +73,59 @@ const JournalContextProvider = ({
     });
   }, [currentSpreadId, setCurrentSpreadItems]);
 
+  // Load all the images
+  React.useEffect(() => {
+    if (!journalId) {
+      return;
+    }
+    getImagesForJournalWithUsageInformation(journalId).then((loadedImages) => {
+      console.log("loaded", loadedImages);
+      setLoadedImages(loadedImages);
+    });
+  }, [journalId, setLoadedImages]);
+
+  // Update the loaded images based on changes to the current spread items
+  // A bit hacky
+  // TODO: see if there's a more elegant way
+  React.useEffect(() => {
+    console.log("udating images or at least attempting to", loadedImages);
+    setLoadedImages((loadedImages) => {
+      const imageIdsCurrentlyUsedInSpread = currentSpreadItems.map(
+        (item) => item.imageId
+      );
+
+      // Get the images that had been being used in this spread.
+      const imagesPreviouslyUsedInThisSpread = loadedImages.filter(
+        (image) => image.isUsedBySpreadId === currentSpreadId
+      );
+
+      // Check whether or not it's still being used.
+      for (const loadedImage of imagesPreviouslyUsedInThisSpread) {
+        // If not, update state accordingly.
+        if (!imageIdsCurrentlyUsedInSpread.includes(loadedImage.id)) {
+          loadedImage.isUsedBySpreadId = null;
+          loadedImage.isUsedBySpreadItemId = null;
+        }
+      }
+
+      // Now get all the images used in the spread
+      const imagesCurrentlyUsedInSpread = loadedImages.filter((image) =>
+        imageIdsCurrentlyUsedInSpread.includes(image.id)
+      );
+      for (const loadedImage of imagesCurrentlyUsedInSpread) {
+        loadedImage.isUsedBySpreadId = currentSpreadId;
+        const spreadItem = currentSpreadItems.find(
+          (el) => (el.imageId = loadedImage.id)
+        );
+        if (!spreadItem) {
+          throw new Error("coding error");
+        }
+        loadedImage.isUsedBySpreadItemId = spreadItem.id;
+      }
+      return [...loadedImages];
+    });
+  }, [currentSpreadItems, setLoadedImages]);
+
   return (
     <JournalContext.Provider
       value={{
@@ -69,7 +135,9 @@ const JournalContextProvider = ({
         allSpreads,
         setAllSpreads,
         currentSpreadItems,
-        setCurrentSpreadItems
+        setCurrentSpreadItems,
+        loadedImages,
+        setLoadedImages
       }}
     >
       {children}
--- src/components/PhotoTray/PhotoTray.tsx
+++ src/components/PhotoTray/PhotoTray.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import style from "./PhotoTray.module.css";
 import {
   createImageResourceForJournal,
-  getImagesForJournalWithUsageInformation,
   JournalImage,
 } from "@/helpers/indexdb";
 import { FabricContext } from "../FabricContextProvider";
@@ -15,59 +14,10 @@ type SortBy = "imageDate" | "importTime" | "imageDateReversed";
 
 function PhotoTray() {
   const [fabricCanvas] = React.useContext(FabricContext);
-  const { journalId, currentSpreadId, currentSpreadItems } = React.useContext(JournalContext);
+  const { journalId, loadedImages, setLoadedImages, currentSpreadId, currentSpreadItems } = React.useContext(JournalContext);
 
-  // TODO: This is a lot of copies... Figure out if performance is miserable
-  const [loadedImages, setLoadedImages] = React.useState<Array<JournalImage>>(
-    []
-  );
   const [selectedSortBy, setSelectedSortBy] =
     React.useState<SortBy>("importTime");
-
-  React.useEffect(() => {
-    if (!journalId) {
-      return;
-    }
-    getImagesForJournalWithUsageInformation(journalId).then((loadedImages) => {
-      console.log("loaded", loadedImages);
-      setLoadedImages(loadedImages);
-    });
-  }, [journalId, setLoadedImages]);
-
-  // Update the loaded images based on changes to the current spread items
-  // A bit hacky
-  // TODO: see if there's a more elegant way
-  React.useEffect(() => {
-    console.log('udating images or at least attempting to', loadedImages)
-    setLoadedImages((loadedImages) => {
-      const imageIdsCurrentlyUsedInSpread = currentSpreadItems.map(item => item.imageId);
-
-      // Get the images that had been being used in this spread.
-      const imagesPreviouslyUsedInThisSpread = loadedImages.filter(image => image.isUsedBySpreadId === currentSpreadId);
-
-      // Check whether or not it's still being used.
-      for (const loadedImage of imagesPreviouslyUsedInThisSpread) {
-        // If not, update state accordingly.
-        if (!imageIdsCurrentlyUsedInSpread.includes(loadedImage.id)) {
-          loadedImage.isUsedBySpreadId = null;
-          loadedImage.isUsedBySpreadItemId = null;
-        }
-      }
-
-      // Now get all the images used in the spread
-      const imagesCurrentlyUsedInSpread = loadedImages.filter(image => imageIdsCurrentlyUsedInSpread.includes(image.id))
-      for (const loadedImage of imagesCurrentlyUsedInSpread) {
-        loadedImage.isUsedBySpreadId = currentSpreadId;
-        const spreadItem = currentSpreadItems.find(el => el.imageId = loadedImage.id);
-        if (!spreadItem) {
-          throw new Error('coding error');
-        }
-        loadedImage.isUsedBySpreadItemId = spreadItem.id;
-      }
-      return [...loadedImages];
-    })
-  }, [currentSpreadItems, setLoadedImages])
-  console.log("PHOTO TRAY RENDER", loadedImages);
 
   // TODO: Make this its own component
   let button = <></>;
